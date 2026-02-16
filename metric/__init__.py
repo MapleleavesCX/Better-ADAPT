9810e3499fc90026752de719f7512026d38ec0bf
MapleleavesCX <129497023+MapleleavesCX@users.noreply.github.com>
2026-02-16 16:09:27 +0800

    Delete metric directory

--- metric/__init__.py
+++
@@ -1,5 +0,0 @@
-# 引用.
-from adapt.metric.nc import NeuronCoverage
-from adapt.metric.nbc import NeuronBoundaryCoverage
-from adapt.metric.snac import StrongNeuronActivationCoverage
-from adapt.metric.ssc import SignSignCoverage
